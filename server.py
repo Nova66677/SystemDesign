37de087e3a6b060ea49a90b3bcf65c5278350058
Nova66677 <echopin91@gmail.com>
2024-01-15 15:05:34 +0100

    first push

--- server.py
+++ server.py
@@ -5,12 +5,17 @@ import threading
 host = "127.0.0.1"
 port = 4444
 
-def my_func(t, addr):
-    contenu = t.recv(4096).decode()
+def my_func(c, addr):
+    """
+    :param c: (object socket) notre connection fille entre le serveur et le client
+    :param addr: (tuple) Contient l'adresse et le port de notre connection fille
+    :return: None
+    """
+    contenu = c.recv(4096).decode()
     print(str(addr[0]), "> ", contenu)
     print("Goodbye ! ")
-    t.send(b'Message recu !')
-    t.close()
+    c.send(b'Message recu !')
+    c.close()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host, port))
@@ -21,10 +26,10 @@ i = 0
 while i < 5 :
     s.listen(5)
     c, addr = s.accept()
-    threading.Thread(target=my_func, args=(c,addr)).start()
+    threading.Thread(target=my_func, args=(c,addr)).start() # On créé notre thread qui gère le client arrivé
     i += 1
 
-s.close()
+s.close() # On ferme la connection principale
 
 
 
